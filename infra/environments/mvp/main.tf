02b8d5b26bf0d25ffa072b0164d2858ffed6457f
Eduardo Salinas <117323510+ThankedDeer@users.noreply.github.com>
2026-04-04 00:18:17 -0600

    Add ECR policy module to main Terraform configuration
    - Introduced a new module for ECR policy management in the main.tf file.
    - Configured the module to iterate over defined ECR services, passing necessary variables for project and environment settings.
    - This addition enhances the infrastructure setup by allowing for more granular control over ECR policies.

--- infra/environments/mvp/main.tf
+++ infra/environments/mvp/main.tf
@@ -7,3 +7,15 @@ module "ecr" {
   service_name          = each.key
   image_retention_count = var.ecr_image_retention_count
 }
+
+module "ecr_policy" {
+  source   = "../../modules/ecr-policy"
+  for_each = var.ecr_services
+
+  project               = var.project
+  environment           = var.environment
+  service_name          = each.key
+  image_retention_count = var.ecr_image_retention_count
+}
+
+
